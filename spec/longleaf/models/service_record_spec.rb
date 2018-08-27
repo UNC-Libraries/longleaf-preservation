7326327261a56ca546d27f3ca52913cffee8b52e
bbpennel <bbpennel@email.unc.edu>
2018-08-27 08:28:20 -0400

    Added test for service record

---
+++ spec/longleaf/models/service_record_spec.rb
@@ -0,0 +1,105 @@
+require 'spec_helper'
+require 'longleaf/models/service_record'
+require 'longleaf/models/md_fields'
+
+describe Longleaf::ServiceRecord do
+  
+  let(:record_no_props) {
+    Longleaf::ServiceRecord.new
+  }
+  
+  let(:service_props) { {
+    Longleaf::MDFields::SERVICE_TIMESTAMP => '2018-01-01T01:00:00.000Z',
+    Longleaf::MDFields::STALE_REPLICAS => true,
+    Longleaf::MDFields::RUN_NEEDED => true,
+    'other_prop' => 'value'
+  } }
+  
+  let(:record_with_props) { Longleaf::ServiceRecord.new(service_props) }
+  
+  describe '.properties' do
+    context 'with no properties' do
+      it { expect(record_no_props.properties).to be_empty }
+    end
+    
+    context 'with properties' do
+      subject { record_with_props.properties }
+      
+      it { is_expected.to include(Longleaf::MDFields::SERVICE_TIMESTAMP => '2018-01-01T01:00:00.000Z',
+        Longleaf::MDFields::STALE_REPLICAS => true,
+        Longleaf::MDFields::RUN_NEEDED => true,
+        'other_prop' => 'value' ) }
+      it { expect(subject.length).to eq 4 }
+    end
+  end
+  
+  describe '.run_needed' do
+    context 'with no properties' do
+      it { expect(record_no_props.run_needed).to be_falsey }
+      
+      it "does set property" do
+        record_no_props.run_needed = true
+        expect(record_no_props.run_needed).to be true
+      end
+    end
+    
+    context 'with run-needed property' do
+      it { expect(record_with_props.run_needed).to be true }
+    end
+  end
+  
+  describe '.timestamp' do
+    context 'with no properties' do
+      it { expect(record_no_props.timestamp).to be_nil }
+      
+      it "does set property" do
+        record_no_props.timestamp = '2018-01-01T00:00:00.000Z'
+        expect(record_no_props.timestamp).to eq '2018-01-01T00:00:00.000Z'
+      end
+    end
+
+    context 'with properties' do
+      it { expect(record_with_props.timestamp).to eq '2018-01-01T01:00:00.000Z' }
+    end
+  end
+  
+  describe '.stale_replicas' do
+    context 'with no properties' do
+      it { expect(record_no_props.stale_replicas).to be_falsey }
+      
+      it "does set property" do
+        record_no_props.stale_replicas = true
+        expect(record_no_props.stale_replicas).to be true
+        record_no_props.stale_replicas = false
+        expect(record_no_props.stale_replicas).to be_falsey
+      end
+    end
+    
+    context 'with stale_replicas property' do
+      it { expect(record_with_props.stale_replicas).to be true }
+    end
+  end
+  
+  describe '.[]' do
+    context 'with no properties' do
+      subject { record_no_props }
+      
+      it { expect(subject['key']).to be_nil }
+      
+      it 'does set property' do
+        subject['key'] = 'value'
+        expect(subject['key']).to eq 'value'
+      end
+    end
+    
+    context 'with properties' do
+      subject { record_with_props }
+      
+      it { expect(subject[Longleaf::MDFields::SERVICE_TIMESTAMP]).to eq '2018-01-01T01:00:00.000Z' }
+      it { expect(subject[Longleaf::MDFields::STALE_REPLICAS]).to be true }
+      it { expect(subject[Longleaf::MDFields::RUN_NEEDED]).to be true }
+      it { expect(subject['other_prop']).to eq 'value' }
+    end
+  end
+  
+end
