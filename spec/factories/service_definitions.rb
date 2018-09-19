187a073a4a3b14bb0e371f722122eddc166a4313
bbpennel <bbpennel@email.unc.edu>
2018-09-19 10:23:06 -0400

    Added ServiceManager and associated test

--- spec/factories/service_definitions.rb
+++ spec/factories/service_definitions.rb
@@ -1,6 +1,7 @@
 require 'longleaf/models/service_definition'
 require 'longleaf/services/service_definition_manager'
 require 'longleaf/services/service_mapping_manager'
+require 'longleaf/services/service_manager'
 
 FactoryBot.define do
   factory(:service_definition, class: Longleaf::ServiceDefinition) do
@@ -20,5 +21,16 @@ FactoryBot.define do
     config { {} }
 
     initialize_with { new(config) }
+  end
+  
+  factory(:service_manager, class: Longleaf::ServiceManager) do
+    transient do
+      config { {} }
+    end
+    
+    definitions_manager { build(:service_definition_manager, config: config) }
+    mappings_manager { build(:service_mapping_manager, config: config) }
+
+    initialize_with { new(attributes) }
   end
 end
---
+++ spec/longleaf/services/service_manager_spec.rb
@@ -0,0 +1,61 @@
+require 'spec_helper'
+require 'longleaf/services/service_manager'
+require 'longleaf/specs/config_builder'
+require 'tmpdir'
+
+describe Longleaf::ServiceManager do
+  ConfigBuilder ||= Longleaf::ConfigBuilder
+  
+  describe '.initialize' do
+    it 'fails with missing parameters' do
+      expect { Longleaf::ServiceManager.new }.to raise_error(ArgumentError)
+    end
+    
+    it 'fails with nil parameters' do
+      expect { build(:service_mapping_manager, definitions_manager: nil, mappings_manager: nil) }.to raise_error(ArgumentError)
+    end
+  end
+  
+  describe '.list_service_definitions' do
+    context 'with empty sections' do
+      let(:config) { ConfigBuilder.new
+          .with_services
+          .with_locations
+          .with_mappings.get }
+      let(:manager) { build(:service_manager, config: config) }
+      
+      it 'returns nothing' do
+        expect(manager.list_service_definitions(location: 'loc1')).to be_empty
+      end
+    end
+    
+    context 'with mappings' do
+      let(:md_dir) { Dir.mktmpdir('metadata') }
+      let(:path_dir) { Dir.mktmpdir('path') }
+      let(:config) { ConfigBuilder.new
+          .with_services
+          .with_service(name: 'serv1')
+          .with_service(name: 'serv2')
+          .with_locations
+          .with_location(name: 'loc1', path: path_dir, md_path: md_dir)
+          .map_services('loc1', ['serv1', 'serv2'])
+          .get }
+      let(:manager) { build(:service_manager, config: config) }
+      
+      after(:each) do
+        FileUtils.rmdir([md_dir, path_dir])
+      end
+      
+      it 'returns services for loc1' do
+        result = manager.list_service_definitions(location: 'loc1')
+        expect(result.length).to eq 2
+        expect(result[0].name).to eq 'serv1'
+        expect(result[1].name).to eq 'serv2'
+      end
+      
+      it 'returns empty list for unmapped location' do
+        expect(manager.list_service_definitions(location: 'imaginary_place')).to be_empty
+      end
+    end
+  end
+end
