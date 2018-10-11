3ca11ffcbc6af5c102653c4fc17ecdc29138c6e5
bbpennel <bbpennel@email.unc.edu>
2018-10-11 10:39:53 -0400

    Add redirecting logger class

---
+++ lib/longleaf/logging.rb
@@ -0,0 +1,26 @@
+require 'longleaf/logging/redirecting_logger'
+
+module Longleaf
+  module Logging
+    # Get the main logger for longleaf
+    def logger
+      Logging.logger
+    end
+    
+    # Get the main logger for longleaf
+    def self.logger
+      @logger ||= RedirectingLogger.new
+    end
+  
+    def initialize_logger(failure_only, log_level, log_format, datetime_format)
+      Logging.initialize_logger(failure_only, log_level, log_format, datetime_format)
+    end
+  
+    def self.initialize_logger(failure_only, log_level, log_format, datetime_format)
+      @logger = RedirectingLogger.new(failure_only: failure_only,
+          log_level: log_level,
+          log_format: log_format,
+          datetime_format: datetime_format)
+    end
+  end
+end
