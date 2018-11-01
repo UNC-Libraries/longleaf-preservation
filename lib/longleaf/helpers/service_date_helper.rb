4a6aabf1f6fe6c891c5d199894463ad1418fa8cb
bbpennel <bbpennel@email.unc.edu>
2018-11-01 17:58:13 -0400

    Add helper for modifying timestamps based on service frequency/delay modifiers

---
+++ lib/longleaf/helpers/service_date_helper.rb
@@ -0,0 +1,43 @@
+require 'time'
+
+module Longleaf
+  class ServiceDateHelper
+    
+    # Adds the amount of time from modifier to the provided timestamp
+    # @param timestamp [String] ISO-8601 timestamp string
+    # @param modifier [String] amount of time to add to the timestamp. It must follow the syntax
+    #    "<quantity> <time unit>", where quantity must be a positive whole number and time unit
+    #    must be second, minute, hour, day, week, month or year (unit may be plural).
+    #    Any info after a comma will be ignored.
+    # @return [String] the original timestamp in ISO-8601 format with the provided amount of time added.
+    def self.add_to_timestamp(timestamp, modifier)
+      if modifier =~ /^(\d+) *(second|minute|hour|day|week|month|year)s?(,.*)?/
+        value = $1.to_i
+        unit = $2
+      else
+        raise ArgumentError.new("Cannot parse time modifier #{modifier}")
+      end
+      
+      datetime = Time.iso8601(timestamp)
+      case unit
+      when 'second'
+        unit_modifier = 1
+      when 'minute'
+        unit_modifier = 60
+      when 'hour'
+        unit_modifier = 3600
+      when 'day'
+        unit_modifier = 24 * 3600
+      when 'week'
+        unit_modifier = 7 * 24 * 3600
+      when 'month'
+        unit_modifier = 30 * 24 * 3600
+      when 'year'
+        unit_modifier = 365 * 24 * 3600
+      end
+      
+      modified_time = datetime + (value * unit_modifier)
+      modified_time.iso8601
+    end
+  end
+end
---
+++ spec/longleaf/helpers/service_date_helper_spec.rb
@@ -0,0 +1,67 @@
+require 'spec_helper'
+require 'longleaf/helpers/service_date_helper'
+
+describe Longleaf::ServiceDateHelper do
+  ServiceDateHelper ||= Longleaf::ServiceDateHelper
+  
+  describe '#add_to_timestamp' do
+    let(:timestamp) { "2010-10-31T01:00:00Z" }
+    
+    it 'adds 1 second' do
+      expect(ServiceDateHelper.add_to_timestamp(timestamp, "1 second")).to eq "2010-10-31T01:00:01Z"
+    end
+    
+    it 'adds 5 seconds' do
+      expect(ServiceDateHelper.add_to_timestamp(timestamp, "5 seconds")).to eq "2010-10-31T01:00:05Z"
+    end
+    
+    it 'adds 5 minutes' do
+      expect(ServiceDateHelper.add_to_timestamp(timestamp, "5 minutes")).to eq "2010-10-31T01:05:00Z"
+    end
+    
+    it 'adds 25 hours' do
+      expect(ServiceDateHelper.add_to_timestamp(timestamp, "25 hours")).to eq "2010-11-01T02:00:00Z"
+    end
+    
+    it 'adds 2 days' do
+      expect(ServiceDateHelper.add_to_timestamp(timestamp, "2 days")).to eq "2010-11-02T01:00:00Z"
+    end
+    
+    it 'adds 2 weeks' do
+      expect(ServiceDateHelper.add_to_timestamp(timestamp, "2 weeks")).to eq "2010-11-14T01:00:00Z"
+    end
+    
+    it 'adds 2 months' do
+      expect(ServiceDateHelper.add_to_timestamp(timestamp, "2 months")).to eq "2010-12-30T01:00:00Z"
+    end
+    
+    it 'adds 5 years' do
+      expect(ServiceDateHelper.add_to_timestamp(timestamp, "5 years")).to eq "2015-10-30T01:00:00Z"
+    end
+    
+    it 'rejects invalid unit' do
+      expect{ ServiceDateHelper.add_to_timestamp(timestamp, "5 boxys") }.to raise_error(ArgumentError)
+    end
+    
+    it 'rejects non-numeric quantity' do
+      expect{ ServiceDateHelper.add_to_timestamp(timestamp, "many days") }.to raise_error(ArgumentError)
+    end
+    
+    it 'rejects missing quantity' do
+      expect{ ServiceDateHelper.add_to_timestamp(timestamp, "seconds") }.to raise_error(ArgumentError)
+    end
+    
+    it 'rejects non-ISO-8601 timestamp' do
+      expect{ ServiceDateHelper.add_to_timestamp("10/10/2010", "1 second") }.to raise_error(ArgumentError)
+    end
+    
+    it 'adds days with extra parameters' do
+      expect(ServiceDateHelper.add_to_timestamp(timestamp, "5 days, immediate")).to eq "2010-11-05T01:00:00Z"
+    end
+    
+    it 'rejects negative quantity' do
+      expect{ ServiceDateHelper.add_to_timestamp(timestamp, "-2 months") }.to raise_error(ArgumentError)
+    end
+  end
+end
+  
