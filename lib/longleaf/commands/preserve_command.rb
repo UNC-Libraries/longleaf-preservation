b96260a1965eed9477af448c0292dd18a6061257
bbpennel <bbpennel@email.unc.edu>
2019-01-28 16:27:17 -0500

    Make file_selector required for register and preserve commands

--- lib/longleaf/commands/preserve_command.rb
+++ lib/longleaf/commands/preserve_command.rb
@@ -21,12 +21,7 @@ module Longleaf
     # @param file_selector [FileSelector] selector for files to preserve
     # @param force [Boolean] force flag
     # @return [Integer] status code
-    def execute(file_selector: nil, force: false)
-      if file_selector.nil?
-        record_failure("Must provide either file paths or storage locations to preserve")
-        return return_status
-      end
-
+    def execute(file_selector:, force: false)
       begin
         # Perform preserve events on each of the file paths provided
         candidate_locator = ServiceCandidateLocator.new(@app_manager)
--- lib/longleaf/commands/register_command.rb
+++ lib/longleaf/commands/register_command.rb
@@ -18,12 +18,7 @@ module Longleaf
     # @param force [Boolean] force flag
     # @param checksums [Array] array of checksums
     # @return [Integer] status code
-    def execute(file_selector: nil, force: false, checksums: nil)
-      if file_selector.nil?
-        record_failure("Must provide one or more file paths to register")
-        return return_status
-      end
-      
+    def execute(file_selector:, force: false, checksums: nil)
       begin
         # Perform register events on each of the file paths provided
         loop do
