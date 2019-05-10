3a8f20cde027bf814c4de9b5c56fa35fe87f3573
bbpennel <bbpennel@email.unc.edu>
2019-05-10 15:09:08 -0400

    Refactor class_options into shared option groups that can be included per method. Change -c to being optional since it can be provided via an environment variable

--- lib/longleaf/cli.rb
+++ lib/longleaf/cli.rb
@@ -2,6 +2,7 @@ require 'thor'
 require 'yaml'
 require 'longleaf/logging'
 require 'longleaf/errors'
+require 'longleaf/version'
 require 'longleaf/commands/deregister_command'
 require 'longleaf/commands/validate_config_command'
 require 'longleaf/commands/validate_metadata_command'
@@ -16,28 +17,66 @@ module Longleaf
   class CLI < Thor
     include Longleaf::Logging
     
-    class_option(:config, :aliases => "-c",
-        :default => ENV['LONGLEAF_CFG'],
-        :required => true,
-        :desc => 'Absolute path to the application configuration used for this command. By default, the value of the environment variable LONGLEAF_CFG is used.')
-    class_option(:load_path, :aliases => "-I",
-        :desc => 'Specify comma seperated directories to add to the $LOAD_PATH, which can be used to specify additional paths from which to load preservation services.')
-    class_option(:system_config, :aliases => "-y",
-        :default => ENV['LONGLEAF_SYSTEM_CFG'],
-        :required => false,
-        :desc => 'Absolute path to the longleaf system configuration used for this command. By default, the value of the environment variable LONGLEAF_SYSTEM_CFG is used.')
-    # Logging/output options
-    class_option(:failure_only,
-        :type => :boolean,
-        :default => false,
-        :desc => 'Only display failure messages to STDOUT.')
-    class_option(:log_level,
-        :default => 'WARN',
-        :desc => 'Level of logging to send to STDERR, following standard ruby Logger levels. This includes: DEBUG, INFO, WARN, ERROR, FATAL, UNKNOWN. Default is WARN.')
-    class_option(:log_format,
-        :desc => 'Format to use for log information sent to STDERR. Can contain the following parameters, which must be wrapped in %{}: severity, datetime, progname, msg. Default is "%{severity} [%{datetime}]: %{msg}"')
-    class_option(:log_datetime,
-        :desc => 'Format to use for timestamps used in logging to STDERR, following strftime syntax.')
+    # Register a shared method option in a shared option group
+    def self.add_shared_option(name, group, options = {})
+      @shared_groups = {} if @shared_groups.nil?
+      @shared_groups[group] = {} if @shared_groups[group].nil?
+      @shared_groups[group][name] =  options
+    end
+
+    # Add all of the shared options in the specified group as method options
+    def self.shared_options_group(group_name)
+      @shared_groups[group_name].each do |opt_name, opt|
+        option opt_name, opt
+      end
+    end
+    
+    # Config options
+    add_shared_option(
+        :config, :common, {
+              :aliases => "-c",
+              :default => ENV['LONGLEAF_CFG'],
+              :required => false,
+              :desc => 'Absolute path to the application configuration used for this command. By default, the value of the environment variable LONGLEAF_CFG is used. A config path is required for most commands.' })
+    add_shared_option(
+        :load_path, :common, {
+              :aliases => "-I",
+              :desc => 'Specify comma seperated directories to add to the $LOAD_PATH, which can be used to specify additional paths from which to load preservation services.' })
+    add_shared_option(
+        :system_config, :common, {
+              :aliases => "-y",
+              :default => ENV['LONGLEAF_SYSTEM_CFG'],
+              :required => false,
+              :desc => 'Absolute path to the longleaf system configuration used for this command. By default, the value of the environment variable LONGLEAF_SYSTEM_CFG is used.' })
+
+    # Logging options
+    add_shared_option(
+        :failure_only, :common, {
+              :type => :boolean,
+              :default => false,
+              :desc => 'Only display failure messages to STDOUT.' })
+    add_shared_option(
+        :log_level, :common, {
+              :default => 'WARN',
+              :desc => 'Level of logging to send to STDERR, following standard ruby Logger levels. This includes: DEBUG, INFO, WARN, ERROR, FATAL, UNKNOWN.' })
+    add_shared_option(
+        :log_format, :common, {
+              :desc => 'Format to use for log information sent to STDERR. Can contain the following parameters, which must be wrapped in %{}: severity, datetime, progname, msg. Default is "%{severity} [%{datetime}]: %{msg}"' })
+    add_shared_option(
+        :log_datetime, :common, {
+              :desc => 'Format to use for timestamps used in logging to STDERR, following strftime syntax.' })
+    
+    # File selection options
+    add_shared_option(
+        :file, :file_selection, {
+              :aliases => "-f", 
+              :required => false,
+              :desc => 'File or files to perform this operation on. If multiple files are provided, they must be comma separated.' })
+    add_shared_option(
+        :location, :file_selection, {
+              :aliases => "-s",
+              :required => false,
+              :desc => 'Name or comma separated names of storage locations to perform this operation over.' })
     
     desc "register", "Register files with Longleaf"
     method_option(:file, :aliases => "-f", 
@@ -50,8 +89,10 @@ module Longleaf
     method_option(:checksums,
         :desc => %q{Checksums for the submitted file. Each checksum must be prefaced with an algorithm prefix. Multiple checksums must be comma separated. If multiple files were submitted, they will be provided with the same checksums. For example: 
           '--checksums "md5:d8e8fca2dc0f896fd7cb4cb0031ba249,sha1:4e1243bd22c66e76c2ba9eddc1f91394e57f9f83"'})
+    shared_options_group(:common)
     # Register event command
     def register
+      verify_config_provided(options)
       setup_logger(options)
       
       app_config_manager = load_application_config(options)
@@ -81,8 +122,10 @@ module Longleaf
         :type => :boolean, 
         :default => false,
         :desc => 'Force the deregistration of already deregistered files.')
+    shared_options_group(:common)
     # Deregister event command
     def deregister
+      verify_config_provided(options)
       setup_logger(options)
       
       app_config_manager = load_application_config(options)
@@ -93,17 +136,14 @@ module Longleaf
     end
     
     desc "preserve", "Perform preservation services on files with Longleaf"
-    method_option(:file, :aliases => "-f", 
-        :required => false,
-        :desc => 'File or files to preserve. If multiple files are provided, they must be comma separated.')
-    method_option(:location, :aliases => "-s",
-        :required => false,
-        :desc => 'Name or comma separated names of storage locations to preserve.')
+    shared_options_group(:file_selection)
     method_option(:force,
         :type => :boolean, 
         :default => false,
         :desc => 'Force the execution of preservation services, disregarding scheduling information.')
+    shared_options_group(:common)
     def preserve
+      verify_config_provided(options)
       setup_logger(options)
       
       extend_load_path(options[:load_path])
@@ -115,8 +155,10 @@ module Longleaf
     end
     
     desc "validate_config", "Validate an application configuration file, provided using --config."
+    shared_options_group(:common)
     # Application configuration validation command
     def validate_config
+      verify_config_provided(options)
       setup_logger(options)
       extend_load_path(options[:load_path])
       
@@ -124,14 +166,11 @@ module Longleaf
     end
     
     desc "validate_metadata", "Validate metadata files."
-    method_option(:file, :aliases => "-f", 
-        :required => false,
-        :desc => 'File or files of which to validate the metadata. If multiple files are provided, they must be comma separated.')
-    method_option(:location, :aliases => "-s",
-        :required => false,
-        :desc => 'Name or comma separated names of storage locations to validate.')
+    shared_options_group(:file_selection)
+    shared_options_group(:common)
     # File metadata validation command
     def validate_metadata
+      verify_config_provided(options)
       setup_logger(options)
       
       app_config_manager = load_application_config(options)
@@ -141,7 +180,9 @@ module Longleaf
     end
     
     desc "setup_index", "Sets up the structure of the metadata index, if one is configured using the system configuration file provided using the --system_config option. Some index types may require additional steps to be taken by an administrator before hand, such as creating users and databases."
+    shared_options_group(:common)
     def setup_index
+      verify_config_provided(options)
       setup_logger(options)
       
       app_config_manager = load_application_config(options)
@@ -161,7 +202,9 @@ module Longleaf
         :type => :boolean, 
         :default => false,
         :desc => 'Only perform the reindex if the index is known to be stale, generally after an config file change.')
+    shared_options_group(:common)
     def reindex
+      verify_config_provided(options)
       setup_logger(options)
       app_config_manager = load_application_config(options)
       
@@ -183,6 +226,12 @@ module Longleaf
         rescue ConfigurationError => err
           logger.failure("Failed to load application configuration due to the following issue:\n#{err.message}")
           exit 1
+        end
+      end
+      
+      def verify_config_provided(options)
+        if options[:config].nil? || options[:config].empty?
+          raise "No value provided for required options '--config'"
         end
       end
       
